7ddb6c37c3023288a3857a345c165f61660ebb06
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2025-12-16 02:02:58 +0600

    Add files via upload

---
+++ 06. var-vs-let-vs-const-and-run-with-node/index.js
@@ -0,0 +1,16 @@
+// !var vs let vs const
+
+// var
+// var fullName = "Seemanto Kumar Dash";
+// var fullName = "Shubro"
+// console.log(fullName);
+
+// let
+// let fullName = "seemanto Kumar Dash";
+// let fullName2 = "shuvro Kumar Dash";
+// console.log(fullName);
+
+// const
+const fullName = "seemanto Kumar Dash";
+// const fullName = "shuvro Kumar Dash";
+console.log(fullName);
