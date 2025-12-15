5df7a03a69a1087b9bb57380c5988a7afc2e1ffa
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2025-12-16 01:24:57 +0600

    Add files via upload

---
+++ 05. variables/index.js
@@ -0,0 +1,17 @@
+// variable declare 
+// var name = "Seemanto Kumar Dash</br>"
+// document.write(name);
+// name = "Shuvro"; 
+// document.write(name);
+
+// var name = "seemanto<br/>";
+// var age = 29;
+var name,age;
+name = "seemanto";
+age = 22;
+// document.write(name);
+// document.write(age);
+
+
+name = "Seemanto Kumar Dash";
+document.write(name + "<br/>" + age);
