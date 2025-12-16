d31b3c717c9eb46f30c05d8ce43d07dff849b6c1
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2025-12-16 20:33:28 +0600

    JS file added

---
+++ 09. How to add string/index.js
@@ -0,0 +1,8 @@
+// document.write("Seemanto" + " Kumar " + "Dash");
+
+// var firstName = "Seeamanto ";
+// var midName = "Kumar";
+// var lastName = " Dash";
+// var fullName = firstName+midName+lastName;
+var fullName = "Seemanto Kumar Dash"
+document.write("My name is " + fullName);
