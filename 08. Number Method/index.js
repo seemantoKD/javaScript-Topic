a4d62bb25bd44293f4c950a995307792b026e4da
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2025-12-16 19:55:16 +0600

    Add files via upload

---
+++ 08. Number Method/index.js
@@ -0,0 +1,32 @@
+/*
+var num = 20;
+
+// number to string
+num = toString(num);
+
+// string to int
+num = parseInt(num);
+
+// int to float
+num = parseFloat(num);
+
+console.log(typeof(num));
+*/
+
+/*
+// point er por koto ghor dekhte cai
+var num = 20.3678;
+console.log(num.toFixed(1));
+*/
+
+/*
+// total kotota ghor dekhte cai
+var num = 20.3678;
+console.log(num.toPrecision(4));
+*/
+
+// Number Method -> any data type converted to Number
+console.log(typeof (Number("121")));
+// for boolean
+console.log((Number(true)));
+console.log((Number(false)));
