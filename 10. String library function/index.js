611a5738ec00d863f5a680e229fd65091f7bf805
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2025-12-16 21:41:20 +0600

    JS file added

---
+++ 10. String library function/index.js
@@ -0,0 +1,44 @@
+// calculating length of a string
+// var s = "Seemanto";
+// var len = s.length;
+// document.write("Number of character: " + len);
+
+
+// user input
+// var s = prompt("Enter your name: ");
+// var len = s.length;
+// document.write("Number of character: " + len);
+
+
+// usses of charAt() function
+// var s = "Seemanto";
+// var ithchar = s.charAt(5);
+// document.write(ithchar);
+
+
+// toUpperCase and toLowerCase
+// var s = "seeManto";
+// var ans = s.toUpperCase();
+// // var ans = s.toLowerCase();
+// document.write(ans);
+
+
+// concatanetion library function
+// var s1 = "Seemanto ", s2 = "Kumar Dash";
+// var fullName = s1.concat(s2);
+// document.write(fullName);
+
+
+// slice function
+// var s1 = "Seemanto";
+// var ans = s1.slice(0,3);
+// document.write(ans);
+
+// task
+// var firstName = prompt("Enter your firstName: ");
+// var lastName = prompt("Enter your lastName: ");
+// var fullName = firstName + " " + lastName;
+// var ans = fullName.toUpperCase();
+// document.write(fullName + "<br>");
+// document.write(ans + "<br>");
+// document.write(fullName.length);
