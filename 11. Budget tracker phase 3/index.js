540779476be1ab65509159bc3d877d95fd851808
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2025-12-18 10:12:33 +0600

    JS file added

---
+++ 11. Budget tracker phase 3/index.js
@@ -0,0 +1,8 @@
+const userName = 'Seemanto Kumar Dash';
+const income = 3750.25;
+const expense = 2580.5;
+
+console.log('Personal Budget Tracker App');
+console.log('User: ' + userName);
+console.log('Total Income: ' + income);
+console.log('Total Expenses: ' + expense);
