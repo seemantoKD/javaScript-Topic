31b7e557857d05bbafe1b9c14d510131367b5477
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2025-12-19 21:27:29 +0600

    fix JS File Added

--- 14. area Calculator/main.js
+++ 14. area Calculator/main.js
@@ -1,4 +1,5 @@
-var base = number(prompt("Enter Base: "));
-var Height = number(prompt("Enter Base: "));
+var base = Number(prompt("Enter Base: "));
+var Height = Number(prompt("Enter Base: "));
 var area = base * Height;
 document.write("Rectengular area = " + area + '<br>');
+
