fbecd30facfab34b71a317dd8cb1ec8f2306740c
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2025-12-31 10:42:08 +0600

    js file added

---
+++ 23. Ternary Operator/main.js
@@ -0,0 +1,8 @@
+// check weather number is positive or negative or zero
+// using if else
+// if(num > 0) document.write(`${num} is Positive Number`);
+// else if(num < 0) document.write(`${num} is Negative Number`);
+// else document.write(`${num} is Zero`);
+
+// using ternary
+num > 0 ? document.write(`${num} is Positive Number`) : num < 0 ? document.write(`${num} is Negative Number`) : document.write(`${num} is Zero`);
