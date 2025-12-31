67c2b5d0c56b680a6d9151d22b3e3e4947ff72bf
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2025-12-31 10:28:28 +0600

    js file added

---
+++ 19. if,else if, else/main.js
@@ -0,0 +1,13 @@
+// conditional statement : if else if else
+
+// check weather num is even or odd
+// var num = Number(prompt("Enter Number: "));
+// if(num % 2 == 0) document.write(`${num} is Even Number`);
+// else document.write(`${num} is Odd Number`);
+
+
+// check weather number is positive or negative or zero
+var num = Number(prompt("Enter Number: "));
+if(num > 0) document.write(`${num} is Positive Number`);
+else if(num < 0) document.write(`${num} is Negative Number`);
+else document.write(`${num} is Zero`);
