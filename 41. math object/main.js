ff1b3fd7be62ffb8646caa67223184e5621cc657
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2026-01-26 20:47:59 +0600

    math object

---
+++ 41. math object/main.js
@@ -0,0 +1,17 @@
+console.log(Math.sqrt(9));               // 3
+console.log(Math.abs(-5));               // 5
+console.log(Math.sin(2));                 // sin(2 in radians)
+console.log(Math.pow(2, 3));               // 8
+console.log(Math.floor(2.4));             // 2
+console.log(Math.round(3.5));             // 4
+console.log(Math.min(2, 3, 4, 5, 1));         // 1
+console.log(Math.random());               // 0 to <1
+
+console.log(Math.random() * 6);           // 0 to <6 (6 হবে না)
+console.log(Math.floor(Math.random() * 10)); // 0 to 9 (10 হবে না)
+
+// for 1 to any number 
+console.log(Math.floor(Math.random() * 10) + 1); // 1 to 10
+
+// real life example : for ludo (1 to 6)
+console.log(Math.floor(Math.random() * 6) + 1);   // 1 to 6
