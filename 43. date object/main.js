de6869a3e157bb1116f9ceba2a9cdb6a64f7a203
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2026-01-27 00:48:50 +0600

    Date object

---
+++ 43. date object/main.js
@@ -0,0 +1,27 @@
+// default date without customize
+var date = new Date();
+console.log(date);
+
+// only for showing year
+var year = date.getFullYear();
+console.log(year);
+
+// only for showing month
+var month = date.getMonth();
+console.log(month); // 0 means january
+
+// only for showing currentDate
+var currentDate = date.getDate();
+console.log(currentDate);
+
+// only for showing currentDay
+var currentDay = date.getDay();
+console.log(currentDay); // 0 means saturday
+
+// only for showing current hours
+var h = date.getHours();
+console.log(h); // 0 means rat 12 ta
+
+// only for showing current minutes
+var m = date.getMinutes();
+console.log(m);
