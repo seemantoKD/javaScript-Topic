eeb10b3f071e0f1d74cbfcb3073443ce5abfb6d8
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2026-01-27 20:43:19 +0600

    DOM | onclick event

---
+++ 50. event handler and onclick event/main.js
@@ -0,0 +1,30 @@
+function myMsg1(){
+    // alert("Hello World!");
+
+    // var myVar = document.querySelector("#para");
+    // myVar.innerHTML = 'You have clicked button one';
+
+    var myImg = document.querySelector("img");
+    myImg.src = "images/skd.jpg";
+}
+
+function myMsg2(){
+    // alert("Hello World!!");
+
+    // var myVar = document.querySelector("#para");
+    // myVar.innerHTML = 'You have clicked button two';
+
+    var myImg = document.querySelector("img");
+    myImg.src = "images/difference between bs4 and bs5.png";
+}
+
+// function myMsg3(){
+//     alert("Hello there!!");
+// } 
+
+
+/*
+    summary:
+        1. how to work after clicking
+            -> using onclick
+*/
