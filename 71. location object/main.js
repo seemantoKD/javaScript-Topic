1af8b8a8228f773715b68d7ef77d274c60cfa2de
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2026-02-16 19:48:38 +0600

    BOM | location object

---
+++ 71. location object/main.js
@@ -0,0 +1,59 @@
+console.clear();
+
+// console.log(window);
+// console.log(window.location);
+// console.log(location);
+
+// href
+// console.log(location.href);
+
+// protocol
+// console.log(location.protocol);
+
+// hostname
+// console.log(location.hostname);
+
+// port
+// console.log(location.port);
+
+// pathname
+// console.log(location.pathname);
+
+// search
+// document.write(location.search);
+
+// hash
+// document.write(location.hash);
+
+// origin
+// document.write(location.origin);
+
+let locationDiv = document.querySelector('.location-div');
+// console.log(locationDiv);
+
+let p1 = locationDiv.children[0];
+// p1.textContent = location.href;
+
+let p2 = locationDiv.children[1];
+// p2.textContent = location.protocol;
+
+let p3 = locationDiv.children[2];
+// p3.textContent = location.hostname;
+
+let p4 = locationDiv.children[3];
+// p4.textContent = location.port;
+
+let p5 = locationDiv.children[4];
+// p5.textContent = location.pathname;
+
+// let visitButton = document.querySelector('button');
+// visitButton.addEventListener('click', function(){
+//     location.assign("https://github.com/seemantoKD");
+//     visitButton.target = blank;
+// });
+
+// replace()
+// location.replace('https://chatgpt.com/c/69931b2c-243c-83ab-bd0a-04c21e93e41d');
+
+// reload()
+// location.reload('http://127.0.0.1:5500/71.%20location%20object/index.html');
