b9f15892fa955d5d1fb1733c96e022d1bae86d0e
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2026-02-20 18:32:34 +0600

    ES6 | Variable, function, string concat

---
+++ 78. Variable and function in ES6/main.js
@@ -0,0 +1,56 @@
+// let x = 6;
+// if(true){
+//     let x = 5; // let block scope
+// }
+// console.log(x);
+
+// let x = 11;
+// for(let x = 1;x<11;x++){
+//     console.log('inside loop: ' + x); // 1 to 10
+// }
+// console.log('outside loop: ' + x); // 11
+
+// const x = 11;
+// if(true){
+//     x = 10; // const is not changeable
+// }
+
+// old version
+// var x = 25, y = 30;
+// var sum = x + y;
+// console.log('sum is: ' + sum);
+
+// ES6 version
+// let x = 25, y = 30;
+// console.log(`Sum is: ${x + y}`);
+
+// let name = 'Seemanto Kumar Dash';
+// let message = `I am ${name}. I am learning JavaScript.`
+// console.log(message);
+
+// function declare normal case
+// function addition (x,y){
+//     var sum = x + y;
+//     console.log(sum);
+// }
+// addition(10,20); // function calling
+
+
+// ES6 function
+// const functionName = (parameter) => {
+
+// }
+// const addition = (x,y) => {
+//     let sum = x + y;
+//     console.log(sum);
+// }
+// addition(2,3); // ES6 function calling
+
+/*
+    ~ covering topic:
+        1. ES6 -
+            i. variable
+            ii. function
+            iii. concataning string
+
+*/
