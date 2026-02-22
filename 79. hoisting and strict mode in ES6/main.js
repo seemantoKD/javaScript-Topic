0c7abc3eca824e46dddd940f1c17ec29a6d283a3
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2026-02-22 20:34:57 +0600

    ES6 | hoisting and strict mode

---
+++ 79. hoisting and strict mode in ES6/main.js
@@ -0,0 +1,10 @@
+// hoisting -> variable declare korar age
+// variable ta use korai hocche hoisting
+// x = 20;
+// console.log(x);
+// var x; // let and const not work in that case
+
+// strict mode -> js er code secure and clean kore
+// "use strict" // active strict mode
+// x = 20;
+// console.log(x);
