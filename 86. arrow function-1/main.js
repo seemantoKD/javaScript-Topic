5b5ee732e897d2c38e756823cc316e8f375c510e
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2026-03-01 19:55:18 +0600

    ES6 | arrow function part 1

---
+++ 86. arrow function-1/main.js
@@ -0,0 +1,42 @@
+// arrow function
+
+// function display1(){
+//     console.log(`I am display 1`)
+// }
+// display1()
+
+/*
+const or let functionName = (parameter) => {
+        // function body
+    }
+*/
+// const display2 = () => {
+//     console.log(`I am display 2`)
+// }
+// display2()
+
+// function msg1(){
+//     return `Hi, I am a message 1`
+// }
+// console.log(msg1())
+
+// const msg1 = () => {
+//     return `Hi, I am a message 1`
+// }
+// console.log(msg1())
+const msg1 = () => `Hi, I am a message 1`
+// console.log(msg1())
+
+// parameter pass in the arrow function
+let add = (x,y) => {
+    return x + y
+}
+console.log(add(10, 20))
+
+
+/*
+    ~ learning:
+        - how to declare arrow function
+        - arrow function shortcut
+        - parameter with arrow function
+*/
