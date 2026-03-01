1b7993c6aded753c4d5b33c684a7cfa6efed5a25
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2026-03-01 19:38:20 +0600

    ES6 | map and filter

---
+++ 85. map and filter array function/main.js
@@ -0,0 +1,25 @@
+// map
+
+let arr = [1,2,3,4,5]
+
+let sqrArr = arr.map(function(x){
+    return x*x
+})
+// console.log(sqrArr)
+
+// filter
+
+let numbers = [22, 31, 4, 5, 36, 26]
+let newArr = numbers.filter(function(x){
+    return x>10
+})
+console.log(newArr)
+
+
+/*
+    ~ learning:
+        - map er ketre array declare alada vabe kora lage na
+           # map new array return kore
+        - filter condition er upor kaj kore 
+            # filter new array return kore
+*/
