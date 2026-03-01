ea4a4af6f309c6d87fa0e14fc8fa1c8c7a82c48e
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2026-03-01 08:44:26 +0600

    ES6 | for each

---
+++ 84. for each/main.js
@@ -0,0 +1,37 @@
+// for each
+
+let numbers = [1, 2, 3, 4, 5]
+
+// for(let i = 0; i<numbers.length; i++){
+//     console.log(numbers[i])
+// }
+
+// function myFunction(x){
+//     console.log(x)
+// }
+// numbers.forEach(myFunction)
+// numbers.forEach(function(x){
+//     console.log(x)
+// })
+
+// task1 for practise
+let squareNumbers = []
+// numbers.forEach(function(x){
+//     x = x * x;
+//     squareNumbers.push(x)
+// })
+// console.log(squareNumbers)
+
+// task2 for practise
+numbers.forEach(function(x){
+    x += 5;
+    console.log(x)
+})
+
+
+/*
+    ~ learning:
+        - how to apply forEach
+        - forEach with anonymous function
+        - 2 task for practise
+*/
