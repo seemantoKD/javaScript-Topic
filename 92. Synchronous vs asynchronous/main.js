04005030506fa21debc0a078fc16dceae602689d
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2026-03-06 18:17:53 +0600

    ES6 | Synchronous vs asynchronous

---
+++ 92. Synchronous vs asynchronous/main.js
@@ -0,0 +1,23 @@
+// synchronous programming
+// console.log('Task 1');
+// console.log('Task 2');
+// console.log('Task 3');
+// console.log('Task 4');
+// console.log('Task 5');
+
+
+console.log('Task 1');
+// asynchronous method
+const dataLoading = () => {
+    console.log('Task 2 data loading')
+}
+setTimeout(dataLoading, 2000);
+console.log('Task 3');
+console.log('Task 4');
+console.log('Task 5');
+
+
+/*
+    ~ learning:
+        - asynchronous sobar shese call hoy
+*/
