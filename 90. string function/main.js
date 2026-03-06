aef96499fa4218074c78bc411600770b1b6f35ef
Seemanto Kumar Dash <seemantokumardash376@gmail.com>
2026-03-06 10:43:15 +0600

    ES6 | String method

---
+++ 90. string function/main.js
@@ -0,0 +1,11 @@
+// startsWith(searchString, position) -> check a string starts with another string
+let message = 'Today is Friday';
+// console.log(message.startsWith('Today', 0)); // second parameter is optional
+
+// endsWith(searchString, length) -> check a string ends with another string
+// console.log(message.endsWith('Friday',message.length)); // second parameter is optional
+
+// includes(searchString, position) -> check if a string contains another string
+console.log(message.includes('Seemanto')); // second parameter is optional
+
+// all these methods are case sensetive and returns only true or false
